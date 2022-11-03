489dfa3b43c3d0bbb1bdbfbcdb4b3a6b2ea10780
Henrique Moreira <h@serrasqueiro.com>
2022-11-03 11:26:22 +0000

    Interesting funds, PPR, capital invest.

---
+++ src/packages/mintracker/sindexes/wisin.py
@@ -0,0 +1,9 @@
+""" wisin
+	(c) 2022  Henrique Moreira
+"""
+
+ISIN_REF = {
+    "LU0104885248": ["Pictet-Water R EUR"],
+    "LU0366537875": ["Pictet-Sovereign Short-Term Money Market USD R"],
+    "PTYESALM0007": ["NB PPR/OICVM - Fundo de Investimento Imobiliario Aberto de Poupanca Reforma"],
+}
